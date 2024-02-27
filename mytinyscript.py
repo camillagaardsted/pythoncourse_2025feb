a5e9f67495d7b9e99c53d3229e371d2f6cc3e0b6
Student <studentE6MY1@t000.gdazcs.com>
2024-02-27 20:53:54 +0100

    Tiny script from day 1 about running scripts with F5 or F9

---
+++ mytinyscript.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 15:39:18 2024
+
+@author: admin
+"""
+
+# a little python script
+
+height = 170
+# this line is not printed when you run a script with F5
+# only if we explicitly say print
+height
+
+print(2*height)
