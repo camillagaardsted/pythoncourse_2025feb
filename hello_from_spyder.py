a7f107f79848598cd0322bccfbcb46b1924080a0
Student <studentE6MY1@t000.gdazcs.com>
2024-02-27 20:51:02 +0100

    Created our first python script in Spyder editor to say hello from Spyder

---
+++ hello_from_spyder.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 09:51:44 2024
+
+@author: admin
+"""
+# F9 is short cut for executing just one line in the interactive console at the bottom
+print('Hello to Spyder')
